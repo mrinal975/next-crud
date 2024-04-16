fd8df930e6318100a922f05669636513765dafd7
Mrinal Mallik <mrinal@nagorik.tech>
2024-04-16 14:03:47 +0600

    Show data on loop 👍

--- components/TopicList.jsx
+++ components/TopicList.jsx
@@ -1,21 +1,41 @@
 import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
-function TopicList() {
+
+async function getTopics() {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-cache",
+    });
+    const resData = await res.json();
+
+    if (!resData) {
+      throw new Error("Failed to fetch data");
+    }
+    return resData.data;
+  } catch (exception) {
+    console.error("error", exception);
+  }
+}
+async function TopicList() {
+  const topics = await getTopics();
+  console.log("---");
   return (
     <>
-      <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-        <div>
-          <h2 className="font-bold text-2xl">Topic List</h2>
-          <div>Topic Description</div>
+      {topics.map((topic) => (
+        <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+          <div>
+            <h2 className="font-bold text-2xl">{topic.title}</h2>
+            <div>{topic.description}</div>
+          </div>
+          <div className="flex gap-2">
+            <RemoveBtn />
+            <Link href="/editTopic/1">
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <RemoveBtn />
-          <Link href="/editTopic/1">
-            <HiPencilAlt size={24} />
-          </Link>
-        </div>
-      </div>
+      ))}
     </>
   );
 }

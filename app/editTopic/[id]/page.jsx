8c4585f945911b9cf7a27909270b4bcf6004c4ab
Mrinal Mallik <mrinal@nagorik.tech>
2024-04-16 14:57:21 +0600

    update data 🙃

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -1,7 +1,30 @@
 import EditTopicForm from "@/components/EditTopicForm";
+const getTopicById = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:3000/api/topics/${id}`, {
+      cache: "no-cache",
+    });
+    const resData = await response.json();
 
-function EditTopic() {
-  return <EditTopicForm />;
+    if (!resData) {
+      throw new Error("Failed to fetch data");
+    }
+    return resData.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function EditTopic({ params }) {
+  const { id } = params;
+  const topic = await getTopicById(id);
+  return (
+    <EditTopicForm
+      title={topic.title}
+      description={topic.description}
+      _id={id}
+    />
+  );
 }
 
 export default EditTopic;
--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,17 +1,53 @@
-function EditTopicForm() {
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+function EditTopicForm({ title, description, _id }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+  const router = useRouter();
+
+  const formSubmit = async (e) => {
+    e.preventDefault();
+    if (!newTitle || !newDescription) {
+      alert("Please fill in all fields");
+      return;
+    }
+    const res = await fetch(`http://localhost:3000/api/topics/${_id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        newTitle: newTitle,
+        newDescription: newDescription,
+      }),
+    });
+    if (res.ok) {
+      router.push("/");
+    } else {
+      throw new Error("Failed to update topic");
+    }
+  };
   return (
-    <form className="flex flex-col gap-3">
+    <form className="flex flex-col gap-3" onSubmit={formSubmit}>
       <input
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Title"
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
       />
       <input
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Description"
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
       />
       <button
+        type="submit"
         className="
   bg-green-600 font-bold text-white py-3 px-6 w-fit"
       >
